44a843c009279ce10c9a0a5e42c5bbc2e9732898
scottyplex <scottyplexgit@gmail.com>
2021-06-06 19:52:06 -0500

    first commit

---
+++ rockPaperScissors.js
@@ -0,0 +1,55 @@
+const getUserChoice = userInput => {
+  userInput = userInput.toLowerCase();
+  if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+    return userInput;
+  } else {
+    console.log(`${userInput} is not valid`);
+  }
+}
+
+const getComputerChoice = () => {
+  randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
+
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return `${userChoice} vs ${computerChoice} Tie!!!`;
+  }
+  if (userChoice === 'rock') {
+    if (computerChoice === 'scissors'){
+    return `${userChoice} vs ${computerChoice} You Won!!!`;
+  } else {
+    return `${userChoice} vs ${computerChoice} You lost....`;
+    }
+  }
+  if (userChoice == 'scissors') {
+    if (computerChoice === 'paper') {
+    return `${userChoice} vs ${computerChoice} You Won!!!`;
+  } else {
+    return `${userChoice} vs ${computerChoice} You lost....`;
+    } 
+  }
+    if (userChoice == 'paper') {
+      if (computerChoice === 'rock') {
+    return `${userChoice} vs ${computerChoice} You Won!!!`;
+  } else {
+    return `${userChoice} vs ${computerChoice} You lost....`;
+    } 
+  }
+}
+function playGame(userChoice = getUserChoice('paper'), computerChoice = getComputerChoice()) {
+  console.log(`You choose ${userChoice} and the Computer chose ${computerChoice}`)
+  console.log('And the Winner is.........')
+  console.log(determineWinner(userChoice, computerChoice))
+}
+
+playGame()
+
